4478849d01bcd3e54739eb34e60223c2bec56840
WorldDeveloper <matviienkos@gmail.com>
2016-12-01 14:59:32 +0200

    Move method getLightColor int Graphics

--- Library/Color.h
+++ Library/Color.h
@@ -5,18 +5,4 @@ namespace trafficlights {
 	enum class Color{
 		Red = 0x44, Yellow = 0x66, Green = 0x22
 	};
-
-	int getLightColor(Color color) {
-		switch (color)
-		{
-		case Color::Red:
-			return 0xCC;
-		case Color::Yellow:
-			return 0xEE;
-		case Color::Green:
-			return 0xAA;
-		default:
-			return -1;
-		}
-	}
 }
